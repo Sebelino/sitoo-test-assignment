5aa5eec5ceed7dd541a9dd6da32ea21921a52596
Sebastian Olsson <sebelino7@gmail.com>
2022-04-11 20:58:04 +0200

    feat: Add nullable description field to product

--- main.go
+++ main.go
@@ -11,10 +11,11 @@ import (
 )
 
 type Product struct {
-	ProductId uint      `json:"productId" gorm:"primarykey"`
-	Title     string    `json:"title"`
-	Sku       string    `json:"sku"`
-	Created   time.Time `json:"created"`
+	ProductId   uint      `json:"productId" gorm:"primarykey"`
+	Title       string    `json:"title"`
+	Sku         string    `json:"sku"`
+	Created     time.Time `json:"created"`
+	Description *string   `json:"description"`
 }
 
 func (Product) TableName() string {
