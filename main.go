3c65b78602709435962d4bcb131325d9927421ce
Sebastian Olsson <sebelino7@gmail.com>
2022-04-13 19:55:35 +0200

    feat: Remove obsolete insert on application boot

--- main.go
+++ main.go
@@ -3,33 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/Sebelino/sitoo-test-assignment/database"
-	"github.com/Sebelino/sitoo-test-assignment/model"
 	"github.com/Sebelino/sitoo-test-assignment/routers"
-	"gorm.io/gorm"
-	"math/rand"
-	"time"
 )
-
-func makeSku() string {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	n := r1.Intn(10000)
-	return fmt.Sprintf("SCK-%d", n)
-}
-
-func insert(db *gorm.DB) {
-	sku := makeSku()
-	db.Create(&model.Product{
-		Title:   "Awesome socks",
-		Sku:     sku,
-		Created: time.Now(),
-	})
-}
 
 func main() {
 	fmt.Println("Starting server...")
 	db := database.Setup()
-	insert(db)
 
 	router := routers.Setup(db)
 	err := router.Run(":8080")
