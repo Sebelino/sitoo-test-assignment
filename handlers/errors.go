8fca47df6918f23abfe9c653b040982ad14b5995
Sebastian Olsson <sebelino7@gmail.com>
2022-04-13 19:48:24 +0200

    feat: Complain if unexpected error code

--- handlers/errors.go
+++ handlers/errors.go
@@ -1,5 +1,10 @@
 package handlers
 
+import "log"
+
 func dbToCustomErrorCode(dbErrorCode uint16) int {
+	if dbErrorCode > 10000 {
+		log.Fatalf("Unexpected database error code: %d", dbErrorCode)
+	}
 	return 30000 + int(dbErrorCode)
 }
