71f47172b9d1f9c5988d8f55cc97aac343f06391
Sebastian Olsson <sebelino7@gmail.com>
2022-04-14 23:35:54 +0200

    feat: Replace boot sleep with proper healthcheck for db

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"github.com/Sebelino/sitoo-test-assignment/database"
 	"github.com/Sebelino/sitoo-test-assignment/routers"
-	"time"
 )
 
 func main() {
 	fmt.Println("Starting server...")
-	time.Sleep(10 * time.Second)
 	db := database.Setup()
 
 	router := routers.Setup(db)
